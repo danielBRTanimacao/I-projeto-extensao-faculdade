f75f672162c02675e6fff17192f80ca655d3c12e
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-02-23 17:37:07 -0300

    new fonts and creating articles for health area

--- frontend/src/pages/Health.jsx
+++ frontend/src/pages/Health.jsx
@@ -6,21 +6,37 @@ export default () => {
             <main className="container">
                 <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
                     <div class="col-lg-6 px-0">
-                        <h1 class="display-4 fst-italic">
-                            Title of a longer featured blog post
+                        <h1 class="display-4 fw-bold text-success">
+                            Lorem ipsum dolor sit amet.
                         </h1>
                         <p class="lead my-3">
-                            Multiple lines of text that form the lede, informing
-                            new readers quickly and efficiently about what’s
-                            most interesting in this post’s contents.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Ex dolore nulla rem adipisci iure ut totam
+                            itaque corporis fuga voluptatem!
                         </p>
-                        <p class="lead mb-0">
-                            <a href="#" class="text-body-emphasis fw-bold">
+                        <p class="mb-0">
+                            <a href="#" class="text-body-emphasis link-success">
                                 Continue lendo...
                             </a>
                         </p>
                     </div>
                 </div>
+                <section className="row">
+                    <div className="col-md-6">
+                        <div>
+                            <h3>Lorem ipsum dolor sit amet.</h3>
+                            <p>
+                                Lorem ipsum dolor, sit amet consectetur
+                                adipisicing elit. Accusamus, doloribus!
+                                Molestiae nesciunt nam rem obcaecati?
+                            </p>
+                        </div>
+                    </div>
+                    <article className="col-md-6">Area 2 mais sobre</article>
+                </section>
+                <section>
+                    <div>Calculos</div>
+                </section>
             </main>
         </>
     );
