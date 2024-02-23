af78efe3eaa02480ed4e490e2248cedf4239c45d
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-02-23 12:11:30 -0300

    new features init health

--- frontend/src/pages/Health.jsx
+++ frontend/src/pages/Health.jsx
@@ -3,12 +3,25 @@ import React from "react";
 export default () => {
     return (
         <>
-            <section className="container-lg">
-                <div>
-                    <h1 className="display-1">Saúde</h1>
+            <main className="container">
+                <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
+                    <div class="col-lg-6 px-0">
+                        <h1 class="display-4 fst-italic">
+                            Title of a longer featured blog post
+                        </h1>
+                        <p class="lead my-3">
+                            Multiple lines of text that form the lede, informing
+                            new readers quickly and efficiently about what’s
+                            most interesting in this post’s contents.
+                        </p>
+                        <p class="lead mb-0">
+                            <a href="#" class="text-body-emphasis fw-bold">
+                                Continue lendo...
+                            </a>
+                        </p>
+                    </div>
                 </div>
-                <article>Criar um tipo de blog aqui</article>
-            </section>
+            </main>
         </>
     );
 };
