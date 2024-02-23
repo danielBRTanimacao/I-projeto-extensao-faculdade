658a502a145632de96c697eea8fbac73bb3db308
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-02-23 17:50:10 -0300

    testing features

--- frontend/src/pages/Health.jsx
+++ frontend/src/pages/Health.jsx
@@ -4,35 +4,29 @@ export default () => {
     return (
         <>
             <main className="container">
-                <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
-                    <div class="col-lg-6 px-0">
-                        <h1 class="display-4 fw-bold text-success">
+                <div className="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
+                    <div className="col-lg-6 px-0">
+                        <h1 className="display-4 fw-bold text-success">
                             Lorem ipsum dolor sit amet.
                         </h1>
-                        <p class="lead my-3">
+                        <p className="lead my-3">
                             Lorem ipsum dolor sit amet, consectetur adipisicing
                             elit. Ex dolore nulla rem adipisci iure ut totam
                             itaque corporis fuga voluptatem!
                         </p>
-                        <p class="mb-0">
-                            <a href="#" class="text-body-emphasis link-success">
+                        <p className="mb-0">
+                            <a
+                                href="#"
+                                className="text-body-emphasis link-success"
+                            >
                                 Continue lendo...
                             </a>
                         </p>
                     </div>
                 </div>
                 <section className="row">
-                    <div className="col-md-6">
-                        <div>
-                            <h3>Lorem ipsum dolor sit amet.</h3>
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur
-                                adipisicing elit. Accusamus, doloribus!
-                                Molestiae nesciunt nam rem obcaecati?
-                            </p>
-                        </div>
-                    </div>
-                    <article className="col-md-6">Area 2 mais sobre</article>
+                    <div className="col-md-7">Area 1</div>
+                    <article className="col-md-5">Artigos 1</article>
                 </section>
                 <section>
                     <div>Calculos</div>
